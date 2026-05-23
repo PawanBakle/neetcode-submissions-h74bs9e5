870245f95b47a839fb23f7ff3cd996ec4269b762
Pawan Bakle <77939822+PawanBakle@users.noreply.github.com>
2026-05-23 15:14:53 +0530

    Add: is-palindrome - submission-26

---
+++ Data Structures & Algorithms/is-palindrome/submission-26.py
@@ -0,0 +1,19 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+
+        ''' remove all the alphas and then loop till half 
+        to check if vals are same from both ends
+        '''
+        cleaned_s = re.sub(r'\W', '', s)
+
+        left = 0
+        right = len(cleaned_s)-1
+        half = (len(cleaned_s))//2
+        for i in range(0,half):
+            if cleaned_s[left].lower() != cleaned_s[right].lower():
+                return False
+            left += 1
+            right -=1
+        return True
