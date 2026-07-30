2032eed5dab19dbfb24260f51a09c6334f2cfdf0
Pawan Bakle <77939822+PawanBakle@users.noreply.github.com>
2026-07-30 22:14:26 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+        else:
+            return None
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        return None
+
+    def getMin(self) -> int:
+        if self.stack:
+            return min(self.stack)
+        return None
+        
